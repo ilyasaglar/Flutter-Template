47a3648a927c5b605f58b4024faa18fbfbd5667c
ilyas ağlar <ilyas_693@hotmail.com>
2020-03-16 00:08:28 +0300

    Update homelist.dart

--- lib/model/homelist.dart
+++ lib/model/homelist.dart
@@ -37,5 +37,17 @@ class HomeList {
       imagePath: 'assets/design_course/design_course.png',
       navigateScreen: DesignCourseHomeScreen(),
     ),
+    HomeList(
+      imagePath: 'assets/hotel/hotel_booking.png',
+      navigateScreen: HotelHomeScreen(),
+    ),
+    HomeList(
+      imagePath: 'assets/fitness_app/fitness_app.png',
+      navigateScreen: FitnessAppHomeScreen(),
+    ),
+    HomeList(
+      imagePath: 'assets/design_course/design_course.png',
+      navigateScreen: DesignCourseHomeScreen(),
+    ),
   ];
 }
